a52154296bb6baa3dc07ef015a1041d96c22e59f
sedanight <jerenawolunta@gmail.com>
2023-07-30 12:27:05 +0700

    latihan pemula

---
+++ src/server.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
